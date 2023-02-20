c6f488969cbfbbcee97b6cbb41ecf8769566dd26
Animila <khristoforov-i@mail.ru>
2023-02-21 08:45:53 +0900

    Дизайн страницы

--- app/src/main/java/com/example/petshop/PetGetActivity.java
+++ app/src/main/java/com/example/petshop/PetGetActivity.java
@@ -3,11 +3,13 @@ package com.example.petshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -23,9 +25,10 @@ import retrofit2.Response;
 
 public class PetGetActivity extends AppCompatActivity {
 
-    private TextView namePet, idPet;
+    private TextView namePet, idPet, categoryPet, statusPet, tagsPet;
+    private EditText getId;
     private ImageView photoPet;
-    private Button getBtn;
+    private Button getBtn, backBtn;
     private ProgressBar progress;
 
     @Override
@@ -36,18 +39,24 @@ public class PetGetActivity extends AppCompatActivity {
 
         progress = (ProgressBar) findViewById(R.id.progressBar);
         progress.setVisibility(View.INVISIBLE);
-        namePet = (TextView) findViewById(R.id.namePat);
         photoPet = (ImageView) findViewById(R.id.imageView);
         getBtn = (Button) findViewById(R.id.btnLoad);
+        backBtn = (Button) findViewById(R.id.btnStep);
+        getId = (EditText) findViewById(R.id.id_pet);
+
+        namePet = (TextView) findViewById(R.id.namePat);
         idPet = (TextView) findViewById(R.id.id);
+        categoryPet = (TextView) findViewById(R.id.categoryPet);
+        statusPet = (TextView) findViewById(R.id.statusPet);
+        tagsPet = (TextView) findViewById(R.id.tegsPet);
 
         PetService petService = PetService.retrofit.create(PetService.class);
-        SharedPreferences sharedPreferences = getSharedPreferences("Pets", Context.MODE_PRIVATE);
-        Integer myValue = Integer.parseInt(sharedPreferences.getString("new_pet", "123"));
+//        SharedPreferences sharedPreferences = getSharedPreferences("Pets", Context.MODE_PRIVATE);
+//        Integer myValue = Integer.parseInt(sharedPreferences.getString("new_pet", "123"));
 
         getBtn.setOnClickListener(view -> {
 
-            Call<Pet> pet = petService.getPet(myValue);
+            Call<Pet> pet = petService.getPet(Integer.parseInt(getId.getText().toString()));
             progress.setVisibility(View.VISIBLE);
             pet.enqueue(new Callback<Pet>() {
                 @Override
@@ -56,8 +65,12 @@ public class PetGetActivity extends AppCompatActivity {
                         progress.setVisibility(View.INVISIBLE);
 
                         Pet pet = response.body();
-                        idPet.setText("Идентификатор: "+ pet.getId());
+                        idPet.setText("ID: "+ pet.getId());
                         namePet.setText("Имя: "+ pet.getName());
+                        categoryPet.setText("Категория: "+pet.getCategory().getName());
+                        statusPet.setText("Статус: "+ pet.getStatus());
+                        tagsPet.setText("Теги: "+pet.getTags().get(0).getName());
+
                         List<String> petPhoto = pet.getPhotoUrls();
                         Picasso
                                 .get()
@@ -78,6 +91,13 @@ public class PetGetActivity extends AppCompatActivity {
                 }
             });
 
+        });
+
+
+        backBtn.setOnClickListener(v -> {
+            Intent step = new Intent(this, PetNavigationActivity.class);
+            startActivity(step);
+            finish();
         });
     }
 }
